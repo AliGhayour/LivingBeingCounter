86268c9a8c32d5f87dd9ab2a2bba35cc3e70ae65
BitBucketAliGhayourMohatat <ghayour78@yahoo.com>
2020-07-31 19:31:39 +1000

    My first commit

---
+++ WildLifeCounterApplication/Program.cs
@@ -0,0 +1,41 @@
+﻿using ConsoleApp1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static ConsoleApp1.CompositeSanctury;
+
+namespace WildLifeCounterApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var nationalParks = new SingleSanctury("NationalParks", 256);
+            nationalParks.CalculateTotalCount();
+            Console.WriteLine();
+
+            //composite sanctury
+            var rootSanctury = new CompositeSanctury("RootSanctury", 0);
+            var wildRabbit = new SingleSanctury("WildRabbit", 289);
+            var fox = new SingleSanctury("Fox", 587);
+            rootSanctury.Add(wildRabbit);
+            rootSanctury.Add(fox);
+
+            var wildLife = new CompositeSanctury("WildLife", 0);
+            var birds = new SingleSanctury("Birds", 200);
+            var plantspecies = new SingleSanctury("Plantspecies", 150);
+
+            wildLife.Add(birds);
+            wildLife.Add(plantspecies);
+            rootSanctury.Add(wildLife);
+
+
+
+            Console.WriteLine();
+            Console.WriteLine($" Total count of this composite present is: {rootSanctury.CalculateTotalCount()}");
+
+        }
+    }
+}
---
+++ WildLifeCounterApplication/SancturyBase.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public abstract class SancturyBase
+    {
+        protected string _name;
+        protected int _count;
+
+        public SancturyBase(string name, int count)
+        {
+            this._name = name;
+            this._count = count;
+        }
+
+        public abstract int CalculateTotalCount();
+    }
+
+
+    public interface ISancturyOperations
+    {
+        void Add(SancturyBase sanctury);
+        void Remove(SancturyBase sanctury);
+    }
+
+
+    public class CompositeSanctury : SancturyBase, ISancturyOperations
+    {
+        private List<SancturyBase> _sancturies;
+
+        public CompositeSanctury(string name, int count)
+            : base(name, count)
+        {
+            _sancturies = new List<SancturyBase>();
+        }
+
+        public void Add(SancturyBase sanctury)
+        {
+            _sancturies.Add(sanctury);
+        }
+
+        public void Remove(SancturyBase sanctury)
+        {
+            _sancturies.Remove(sanctury);
+        }
+
+        public override int CalculateTotalCount()
+        {
+            int total = 0;
+
+            Console.WriteLine($"{_name} contains the following livingBeings with counts:");
+
+            foreach (var sanctury in _sancturies)
+            {
+                total += sanctury.CalculateTotalCount();
+            }
+
+            return total;
+        }
+
+        public class SingleSanctury : SancturyBase
+        {
+            public SingleSanctury(string name, int count)
+                : base(name, count)
+            {
+            }
+
+            public override int CalculateTotalCount()
+            {
+                Console.WriteLine($"{_name} with the count {_count}");
+
+                return _count;
+            }
+        }
+    }
+}
